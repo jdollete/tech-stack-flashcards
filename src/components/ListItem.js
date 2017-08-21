5e9415a473f249767ddf009f2dca52ce56c3fa07
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-21 17:13:59 -0400

    Render titles onto the screen

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { Card, CardSection } from './common';
 
 class ListItem extends React.Component {
 
@@ -7,10 +9,24 @@ class ListItem extends React.Component {
   }
 
   render() {
+    const { title, description } = this.props.library;
+    const { titleStyle } = styles;
+
     return (
-      
+      <CardSection >
+        <Text style={titleStyle}>
+          {title}
+        </Text>
+      </CardSection>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 15,
+    paddingLeft: 15
+  }
+});
 
 export default ListItem;
