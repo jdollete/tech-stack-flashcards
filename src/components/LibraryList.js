6bebe5890d6cfa0b04bb791c39c9f306ea674817
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-21 13:57:24 -0400

    Create Librarylist  component, connect to redux

---
+++ src/components/LibraryList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Text, StyleSheet } from 'react-native';
+
+class LibraryList extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default connect()(LibraryList);
